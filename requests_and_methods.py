34242f0eff11daaf6f3f3d456efea2832e139031
Aleksandr Zakharov <alexz1983@mail.ru>
2021-06-27 22:07:36 +0300

    ДЗ №7

---
+++ requests_and_methods.py
@@ -0,0 +1,29 @@
+import requests
+
+method = ["GET", "POST", "PUT", "DELETE", "HEAD"]
+
+for i in method:
+    if i == "GET":
+        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=i)
+        print(response.status_code)
+        print(response.text)
+    elif i == "POST":
+        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+        print(response.status_code)
+        print(response.text)
+    elif i == "PUT":
+        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+        print(response.status_code)
+        print(response.text)
+    elif i == "HEAD":
+        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+        print(response.status_code)
+        print(response.text)
+    elif i == "DELETE":
+        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+        print(response.status_code)
+        print(response.text)
+
+response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print(response.status_code)
+print(response.text)
