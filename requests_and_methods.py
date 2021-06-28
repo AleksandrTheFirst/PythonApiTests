74fcd7a5fa1862b1ed5d919770c80fb21616e7d7
Aleksandr Zakharov <alexz1983@mail.ru>
2021-06-28 09:27:00 +0300

    ДЗ №7

--- requests_and_methods.py
+++ requests_and_methods.py
@@ -3,28 +3,27 @@ import requests
 method = ["GET", "POST", "PUT", "DELETE", "HEAD"]
 
 for i in method:
-    if i == "GET":
-        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=i)
-        print(response.status_code)
-        print(response.text)
-    elif i == "POST":
-        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
-        print(response.status_code)
-        print(response.text)
-    elif i == "PUT":
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
-        print(response.status_code)
-        print(response.text)
-    elif i == "HEAD":
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
-        print(response.status_code)
-        print(response.text)
-    elif i == "DELETE":
-        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
-        print(response.status_code)
-        print(response.text)
-    else:
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type")
-        print(response.status_code)
-        print(response.text)
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=i)
+    print(f"This is GET request with parameter = {i}")
+    print(response.status_code)
+    print(response.text)
 
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+    print(f"This is POST request with parameter = {i}")
+    print(response.status_code)
+    print(response.text)
+
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+    print(f"This is PUT request with parameter = {i}")
+    print(response.status_code)
+    print(response.text)
+
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i)
+    print(f"This is DELETE request with parameter = {i}")
+    print(response.status_code)
+    print(response.text)
+
+response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print("This is PUT request without parameters.")
+print(response.status_code)
+print(response.text)
