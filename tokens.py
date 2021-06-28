bc70786cc47891bcd9d14a9796ed957b552d694d
Aleksandr Zakharov <alexz1983@mail.ru>
2021-06-28 11:22:20 +0300

    ДЗ №8

--- tokens.py
+++ tokens.py
@@ -2,7 +2,6 @@ import requests
 import time
 
 response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
-print(response.text)
 
 obj = response.json()
 
@@ -11,8 +10,28 @@ seconds = obj['seconds']
 seconds_job_is_done = seconds + 1
 
 time.sleep(seconds - 1)
-response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=token)
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
 obj = response.json()
-print(obj)
+status = obj['status']
+print(status)
+
+if status == "Job is NOT ready":
+    time.sleep(2)
+    response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
+    obj = response.json()
+    if obj['result'] == "42":
+        if obj['status'] == "Job is ready":
+            print(obj['status'])
+else:
+    response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
+    obj = response.json()
+    if obj['result'] == "42":
+        if obj['status'] == "Job is ready":
+            print(obj['status'])
+        else:
+            print("Job is NOT completed!")
+    else:
+        print("Result is NOT 42!")
+
 
 
