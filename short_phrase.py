688f4b0fa58e34884565def51abfb64d774490e3
Aleksandr Zakharov <alexz1983@mail.ru>
2021-07-05 15:40:41 +0300

    ДЗ №10

--- short_phrase.py
+++ short_phrase.py
@@ -1,6 +1,11 @@
-class ShortPhrase:
+class TestPhrase:
     def test_check_phrase(self):
         phrase = input("Set a phrase: ")
         phrase_len = len(phrase)
-        if phrase_len < 15:
-            assert len(phrase) < 15, f"The phrase = {phrase} is 15 symbols length or more."
+        assert phrase_len < 15, f"The phrase = {phrase} is 15 symbols length or more."
+
+
+
+
+
+
