b1e41af6847201adc4c87a50decd021a8b2b6113
alexz1983 <alexz1983@mail.ru>
2021-07-13 13:13:18 +0300

    test_auth

---
+++ tests/test_user_auth.py
@@ -0,0 +1,31 @@
+import requests
+from lib.base_case import BaseCase
+
+class TestUserAuth(BaseCase):
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa/api/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+    def test_auth_user(self):
+        response2 = requests.get(
+            "https://playground.learnqa/api/user/auth",
+            headers={"x-csrf-token", self.token},
+            cookies={"auth_sid": self.auth_sid}
+        )
+        assert "user_id" in response2.json(), f"There is no user id in the second response"
+        user_id_from_check_method = response2.json()['user_id']
+        print(user_id_from_check_method)
+
+        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
